d94f6a155d1da8286daf17d6a8645e1ca5882db3
heisid <weird_alien@protonmail.com>
2019-09-10 11:34:30 +0700

    working on about page

--- app/src/main/java/io/github/heisid/solarsystemcheatsheet/MainActivity.java
+++ app/src/main/java/io/github/heisid/solarsystemcheatsheet/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -47,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
     public void setMode(int selectedMode) {
         switch (selectedMode) {
             case R.id.about:
+                Intent intentAbout = new Intent(MainActivity.this, About.class);
+                startActivity(intentAbout);
                 break;
             case R.id.exit:
                 break;
